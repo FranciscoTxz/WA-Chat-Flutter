786698df842528192fc08a929d5a8faf1eebd4c2
FranciscoTxz <angelftv2002@hotmail.com>
2025-03-03 08:18:49 -0600

    New Home Configuration

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -3,7 +3,7 @@ import 'package:simple_menu/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(routes: [
-  GoRoute(path: "/", builder: (context, state) => const HomeScreen(), routes: [
+  GoRoute(path: "/", builder: (context, state) => HomeScreen(), routes: [
     /*GoRoute(
       path: "login_screen",
       builder: (context, state) => const LoginScreen(),
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:simple_menu/screens/screens.dart';
 import 'package:simple_menu/widgets/widgets.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
-  final PageController _pageController = PageController();
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-    _pageController.animateToPage(
-      index,
-      duration: Duration(milliseconds: 300),
-      curve: Curves.easeInOut,
-    );
+  Widget _getScreen(int index) {
+    switch (index) {
+      case 0:
+        return ChatScreen();
+      case 1:
+        return StatusScreen();
+      case 2:
+        return CallsScreen();
+      default:
+        return ChatScreen();
+    }
   }
 
   PreferredSizeWidget _getAppBar(int index) {
@@ -37,24 +37,18 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF1C1C1C),
       appBar: _getAppBar(_selectedIndex),
-      body: PageView(
-        controller: _pageController,
-        onPageChanged: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        children: [
-          ChatScreen(changeTab: _onItemTapped),
-          StatusScreen(),
-          CallsScreen(),
-        ],
-      ),
+      body: _getScreen(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
